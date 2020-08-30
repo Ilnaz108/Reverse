06aec5a4070a4df8606a032433229c3e65939251
Ilnaz Minniakhmetov <i.minniahmetov.stc@innopolis.university>
2020-08-30 17:53:56 +0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,24 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        String[] a = {"Харе ", " Кришна ", " Харе ", " Рама "};
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+        System.out.println();
+
+        int n = a.length;
+        String temp;
+
+        for (int i = 0; i < n / 2; i++) {
+            temp = a[n - i - 1];
+            a[n - i - 1] = a[i];
+            a[i] = temp;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+    }
+}
